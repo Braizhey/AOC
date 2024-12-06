7728ebc820c4815b6d4b4b51300f0a05784ab71c
Vincent Thébault <gudule.vincent@gmail.com>
2024-12-06 17:43:22 +0100

    day 6 (preview)

---
+++ AOC.App2024/Resolvers/Day6/ResolverDay6Part1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AOC.Common.Services;
+
+namespace AOC.App2024.Resolvers.Day6
+{
+    public class ResolverDay6Part1 : IAdventServiceResolver
+    {
+        public int Resolve(List<string> data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
